400c170d14f129b9b6c982a868abc6fbf2549263
Håkon Ødegård Løvdal <hakloev@gmail.com>
2015-09-02 14:09:26 +0200

    Added bash script

---
+++ ps1/derp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+P=( 1 2 4 8 )
+N=( 100 $((10**6)) $((10**9)) )
+
+for proc in "${P[@]}"
+do 
+	for interval in "${N[@]}"
+	do
+		increment=$(($interval / 20))
+		for i in {1..20}
+		do
+			end=$(($increment * i))
+			read TIME <<< $(./parallel 2 $end)
+			echo "$proc, $end, ${TIME}" >> result.txt
+		done
+	done
+done
