e9f6851b7d0874443dde34b71a95b057f1f06ab6
Håkon Ødegård Løvdal <hakloev@gmail.com>
2015-09-11 13:35:00 +0200

    Added working code, but need to comment and completely understand

--- ps2/lenna/lenna.cu
+++ ps2/lenna/lenna.cu
@@ -1,29 +1,50 @@
 #include <iostream>
 #include "lodepng.h"
 
+/* Kernel for inverting usigned char */
+__global__ void invert_image( unsigned char *image ) {
+    int index = threadIdx.x + blockIdx.x * blockDim.x; 
+    image[index] = ~image[index]; /* Invert unsigned char */
+}
+
+# define THREADS_PER_BLOCK 512
+
 int main( int argc, char ** argv){
 
-  size_t pngsize;
-  unsigned char *png;
-  const char * filename = "lenna512x512_inv.png";
-  /* Read in the image */
-  lodepng_load_file(&png, &pngsize, filename);
- 
-  unsigned char *image;
-  unsigned int width, height;
-  /* Decode it into a RGB 8-bit per channel vector */
-  unsigned int error = lodepng_decode24(&image, &width, &height, png, pngsize);
+    size_t pngsize;
+    unsigned char *png;
+    const char * filename = "lenna512x512_inv.png";
+    /* Read in the image */
+    lodepng_load_file(&png, &pngsize, filename);
 
-  /* Check if read and decode of .png went well */
-  if(error != 0){
-      std::cout << "error " << error << ": " << lodepng_error_text(error) << std::endl;
-  }
+    unsigned char *image; /* Host copy of image */
+    unsigned int width, height;
+    /* Decode it into a RGB 8-bit per channel vector */
+    unsigned int error = lodepng_decode24(&image, &width, &height, png, pngsize);
 
-  // Do work
+    /* Check if read and decode of .png went well */
+    if(error != 0){
+        std::cout << "error " << error << ": " << lodepng_error_text(error) << std::endl;
+    }
+	
+    // Do work
+	unsigned char *device_image;
+	int loop_size = width * height * 3;
+	size_t size = loop_size * sizeof( unsigned char );
 
-  /* Save the result to a new .png file */
-  lodepng_encode24_file("lenna512x512_orig.png", image , width,height);
+	cudaMalloc( ( void** ) &device_image, size );
+	cudaMemcpy( device_image, image, size, cudaMemcpyHostToDevice );
 
-  return 0;
+    invert_image<<<loop_size / THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>( device_image );
+	
+    cudaMemcpy( image, device_image, size, cudaMemcpyDeviceToHost );
+
+    /* Save the result to a new .png file */
+    lodepng_encode24_file("lenna512x512_orig.png", image , width,height);
+
+	free( image );
+	cudaFree( device_image );
+
+    return 0;
 }
 
